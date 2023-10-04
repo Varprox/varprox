e5462a54459fd7b7f591cf88de2dde6b234b14f6
Richard <frederic.richard@univ-amu.fr>
2023-10-04 13:50:54 +0200

    nettoyage

--- varprox/__init__.py
+++ varprox/__init__.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 __author__ = "Paul Escande and Frédéric Richard."
-__copyright__ = "2021."
+__copyright__ = "2023."
 __credits__ = ["Paul Escande and Frédéric Richard"]
 __license__ = ""
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __maintainer__ = "Paul Escande and Frédéric Richard"
 __email__ = "[paul.escande, frederic.richard]_at_univ-amu.fr"
 __status__ = "Development"
