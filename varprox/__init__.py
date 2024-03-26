c5dda9a69db2be1dfd2f9236465a9684c15668c7
Richard <frederic.richard@univ-amu.fr>
2024-03-26 10:27:19 +0100

    reorganisation des fichiers

--- varprox/__init__.py
+++ varprox/__init__.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = "Paul Escande and Frédéric Richard."
-__copyright__ = "2023."
-__credits__ = ["Paul Escande and Frédéric Richard"]
+__author__ = "Arthur Marmin and Frédéric Richard."
+__copyright__ = "2024."
+__credits__ = ["Arthur Marmin and Frédéric Richard"]
 __license__ = ""
 __version__ = "0.0.2"
-__maintainer__ = "Paul Escande and Frédéric Richard"
-__email__ = "[paul.escande, frederic.richard]_at_univ-amu.fr"
+__maintainer__ = "Arthur Marmin and Frédéric Richard"
+__email__ = "[arthur.marmin, frederic.richard]_at_univ-amu.fr"
 __status__ = "Development"
 
 """
 Import the main class of the package.
 """
-from varprox.main import Minimize, Varprox_Param, tv
-from varprox.ParamsReader import ParamsReader
+from varprox._minimize import Minimize
+from varprox._parameters import Parameters
+
