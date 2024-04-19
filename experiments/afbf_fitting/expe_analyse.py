77727f2c10cd7243d90f0ea8e91f19ff9bc64e96
Richard <frederic.richard@univ-amu.fr>
2024-04-19 10:26:01 +0200

    afbf fitting

---
+++ experiments/afbf_fitting/expe_analyse.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+r"""Fitting variogram of an anisotropic fractional Brownian field:
+    Part 3. Analysis of experiments.
+"""
+import numpy as np
+from afbf.Simulation.TurningBands import LoadTBField
+
+# Repetory for data
+home_dir = "/home/frichard/Recherche/Python/varprox/"
+home_dir = "C:/Users/frede/Nextcloud/Synchro/Recherche/Python/varprox/"
+data_in = "data/afbf_fitting/"
+data_out = "experiments/afbf_fitting/results/"
+
+
+def CompareModels(model_ref, model_est):
+    """Model Comparison.
+    """
+
+    t = np.linspace(- np.pi / 2, np.pi / 2, 10000)
+    hurst_ref = model_ref.hurst
+    hurst_est = model_est.hurst
+
+    hurst_ref.Evaluate(t)
+    hurst_est.Evaluate(t)
+
+    rmse = np.sqrt(np.mean(np.power(hurst_ref.values - hurst_est.values, 2)))
+
+    return rmse
+
+
+for expe in range(1):  # Nbexpe):
+    caseid = str(expe + 100)
+    caseid = caseid[1:]
+    file_simu = home_dir + data_in + caseid
+    file_res = home_dir + data_out + caseid
+
+    model_ref = LoadTBField(file_simu)
+    model_varproj = LoadTBField(file_res + "-varproj")
+
+    rmse = CompareModels(model_ref, model_varproj)
+    print(rmse)
---
+++ experiments/afbf_fitting/param_expe.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class params:
+    # Number of experiments
+    Nbexpe: int = 10
+    # Number of parameters for the Hurst and topothesy functions
+    hurst_dim: int = 32
+    topo_dim: int = 32
+    # Image size.
+    N: int = 512
+    # Enhancement factor for semi-variogram values.
+    enhan_factor: float = 10e4
+    # True if the the theoretical semi-variogram is fitted
+    Tvario: bool = False
+    # Size of the grid for the definition of the semi-variogram
+    grid_dim: int = 40
+    # Step for grid definition
+    grid_step: int = 2
+    # Size of field realization
+    grid_normalization: int = 100
+    # Display results of each experiment
+    display = False
+    # Save the results
+    save = False
+    # 1 if model with noise and 0 otherwise
+    noise = 1
+    # True if the multigrid algorithm is used.
+    multigrid = False
