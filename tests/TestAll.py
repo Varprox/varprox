c4779212a0cbb87e358b8784630ad9b6a0069de3
Richard <frederic.richard@univ-amu.fr>
2025-09-30 10:31:16 +0200

    nettoyage

--- tests/TestAll.py
+++
@@ -1,7 +0,0 @@
-"""Code tests for the whole package varprox.
-
-.. codeauthor:: Frédéric Richard <frederic.richard_at_univ-amu.fr>
-"""
-
-import doctest
-doctest.testfile("test_semi_variograms.rst")
