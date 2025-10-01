433d4a751cbe476d4b9085da233063ae6bb87b99
Richard <frederic.richard@univ-amu.fr>
2025-10-01 12:29:44 +0200

    copyright entetes

--- examples/VarProx-Paper-2025/expe_fit.py
+++ examples/VarProx-Paper-2025/expe_fit.py
@@ -1,4 +1,42 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# ######### COPYRIGHT #########
+# Credits
+# #######
+#
+# Copyright(c) 2025-2025
+# ----------------------
+#
+# * Institut de Mathématiques de Marseille <https://www.i2m.univ-amu.fr/>
+# * Université d'Aix-Marseille <http://www.univ-amu.fr/>
+# * Centre National de la Recherche Scientifique <http://www.cnrs.fr/>
+#
+# Contributors
+# ------------
+#
+# * `Arthur Marmin <mailto:arthur.marmin@univ-amu.fr>`_
+# * `Frédéric Richard <mailto:frederic.richard@univ-amu.fr>`_
+#
+#
+# * This module is part of the package Varprox.
+#
+# Licence
+# -------
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# ######### COPYRIGHT #########
 r"""Fitting variogram of an anisotropic fractional Brownian field:
     Part 2. Variogram fitting.
 """
