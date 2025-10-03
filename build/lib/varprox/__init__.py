999184ccb35d48661734e8ee0df4b8b5495bdc74
Arthur Marmin <arthur.marmin@univ-amu.fr>
2025-10-03 12:27:11 +0200

    Merge

--- build/lib/varprox/__init__.py
+++ build/lib/varprox/__init__.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 __author__ = "Arthur Marmin and Frédéric Richard."
-__copyright__ = "2024."
+__copyright__ = "2025."
 __credits__ = ["Arthur Marmin and Frédéric Richard"]
 __license__ = ""
-__version__ = "0.0.2"
+__version__ = "1.0.1"
 __maintainer__ = "Arthur Marmin and Frédéric Richard"
 __email__ = "[arthur.marmin, frederic.richard]_at_univ-amu.fr"
 __status__ = "Development"
@@ -14,6 +14,4 @@ __status__ = "Development"
 Import the main class of the package.
 """
 from varprox._minimize import Minimize
-from varprox._minimize2D import Minimize2D
 from varprox._parameters import Parameters
-
